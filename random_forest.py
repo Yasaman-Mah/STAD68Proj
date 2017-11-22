7c45d541b2b198998020b8a6dce0810a19226491
Yasaman-Mah <yassaman@bitbucket.org>
2017-11-22 15:55:13 -0500

    plotting precision recall curve for random forest.

--- random_forest.py
+++ random_forest.py
@@ -1,23 +1,39 @@
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 from sample import *
+from sklearn.metrics import precision_recall_curve, average_precision_score
+import matplotlib.pyplot as plt
 
 train_fraud = np.load('train_fraud.npy')
 train_legitimate = np.load('train_legitimate.npy')
 
 # will not feed data in batches
-samp = get_sample(train_legitimate, train_fraud, 10000, 100)
+samp = get_sample(train_legitimate, train_fraud, 10000, 200)
 
 rfc = RandomForestClassifier(class_weight={0:2, 1:1})
 rfc.fit(samp[:,0:30], samp[:,30])
 
 print(rfc.score(samp[:,0:30], samp[:,30]))
 
-#test_fraud = np.load('test_fraud.npy')
-#test_legitimate = np.load('test_legitimate.npy')
+test_fraud = np.load('test_fraud.npy')
+test_legitimate = np.load('test_legitimate.npy')
 
-#test_samp = get_sample(test_legitimate, test_fraud, 1000, 300, replc=True)
+test_data = np.concatenate((test_legitimate, test_fraud))
 
-#print(rfc.score(test_samp[:,0:30], test_samp[:,30]))
+probabilities = rfc.predict_proba(test_data[:,0:30])
+print(sum(probabilities[:,0]), test_data[0,30], rfc.classes_, "num classes", rfc.n_classes_)
+
+precision_rf, recall_rf, thresholds_rf = precision_recall_curve(test_data[:,30], probabilities[:,1])
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.step(recall_rf, precision_rf)
+ax.set_xlabel('recall')
+ax.set_ylabel('precision')
+ax.set_title('precision recall curve for random forest (Gini loss)')
+plt.show()
+
+
+
 
 
